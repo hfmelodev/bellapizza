9a35bd1115b76dac95a728f2b58bc285a39d0122
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2025-01-26 21:29:10 -0300

    Gráfico de produtos populares

--- src/components/app/dashboard/revenue-chart.tsx
+++ src/components/app/dashboard/revenue-chart.tsx
@@ -1,4 +1,11 @@
-import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
 
 import {
   Card,
@@ -48,6 +55,8 @@ export function RevenueChart() {
                 })
               }
             />
+
+            <CartesianGrid vertical={false} className="stroke-muted" />
 
             <Line
               stroke={colors.white}
